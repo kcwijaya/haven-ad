05d4ef56009295f6733cb8b7534a5bf27f2af296
Kimberly Wijaya <kcwijaya@stanford.edu>
2017-02-23 23:05:28 -0800

    Added share and sign up

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ var http = require('http').Server(app);
 var fs = require('fs');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
+
 app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+ });
+
+app.get('/index.html', function(req, res) {
     res.sendFile(__dirname + '/index.html');
  });
 
@@ -15,9 +23,44 @@ app.get('/main.css', function(req, res) {
     res.sendFile(__dirname + '/main.css');
 });
 
+app.get('/images/*', function(req, res)
+{
+  res.sendFile(__dirname + req.url);
+});
 
+app.get('emails.txt', function(req, res)
+{
+  res.sendFile(__dirname + '/emails.txt');
+});
+
+app.get('/signup.html', function(req, res){
+  console.log("hello");
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.get('/invite.html', function(req, res){
+    res.sendFile(__dirname + '/invite.html');
+});
 
 http.listen(process.env.PORT || 8000, function() {
     console.log('listening on ' + process.env.PORT || 8000);
 });
+
+app.post('/sign-up', function(req, res){
+  console.log("omg");
+  console.log(req.body);
+  var email = req.body.email;
+  fs.appendFile('emails.txt', email +"\r\n", (err)=>{
+    if (err)
+    {
+      console.log(err);
+      res.sendStatus(500);
+
+    }
+    console.log(email + " signed up.");
+    res.sendStatus(200);
+
+  });
+});
+
 
